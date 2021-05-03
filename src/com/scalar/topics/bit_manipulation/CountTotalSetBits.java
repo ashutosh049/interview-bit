cf4041267014a94cc3b0256c3747102a60737625
ashutosh049 <ashutosh049@outlook.com>
2021-05-03 11:24:45 +0530

    More programs

--- src/com/scalar/topics/bit_manipulation/CountTotalSetBits.java
+++ src/com/scalar/topics/bit_manipulation/CountTotalSetBits.java
@@ -90,9 +90,10 @@ package com.scalar.topics.bit_manipulation;
 public class CountTotalSetBits {
 
   public static void main(String[] args) {
-    int num = 5;
-    long totalSetBits = solve(5);
-    System.out.println("totalSetBits  = " + totalSetBits);
+    int num = 9;
+    //System.out.println("totalSetBits  for A="+num+" :" + solve_scalar_editorial(num));
+    num = 1;
+    System.out.println("totalSetBits from 1 to "+num+": " + solve_scalar_editorial(num));
   }
 
   public static long solve(int num) {
@@ -127,6 +128,116 @@ public class CountTotalSetBits {
       count += (num % power) - power / 2;
     } else {
       count += 0;
+    }
+
+    return count % 1000000007;
+  }
+
+  /**
+   * If we observe bits from rightmost side at distance i than bits get inverted after 2i position
+   * in vertical sequence.
+   * <p>
+   * For example A = 5; 0 = 0000 1 = 0001 2 = 0010 3 = 0011 4 = 0100 5 = 0101
+   * <p>
+   * So, we will iterate till the number of bits in the number. And we don’t have to iterate every
+   * single number in the range from 1 to A. We will perform the following operations to get the
+   * desired result.
+   * <ul>
+   *
+   * <li>First of all, we will add 1 to the number in order to compensate 0. As the binary number
+   * system starts from 0. So now A = A + 1.
+   * <li>We will keep the track of the number of set bits encountered till now. And we will
+   * initialise it with A/2.
+   * <li>We will keep one variable which is a power of 2, in order to keep track of bit we are
+   * computing.
+   * <li>We will iterate till the power of 2 becomes greater than A.
+   * <li>We can get the number of pairs of 0s and 1s in the current bit for all the numbers by
+   * dividing A by current power of 2.
+   * <li>Now we have to add the bits in the set bits count. We can do this by dividing the
+   * number of pairs of 0s and 1s by 2 which will give us the number of pairs of 1s only and after
+   * that, we will multiply that with the current power of 2 to get the count of ones in the
+   * groups.
+   * <li>Now there may be a chance that we get a number as number of pairs, which is somewhere in
+   * the middle of the group i.e. the number of 1s are less than the current power of 2 in that
+   * particular group. So, we will find modulus and add that to the count of set bits.
+   * </ul>
+   * Time Complexity : O(logA)
+   *
+   * Example:
+   * num= 3
+   * 0: 0000
+   * 1: 0001
+   * 2: 0010
+   * 3: 0011
+   * ------------
+   * num = 3+1= 4
+   * -------
+   * p=2,
+   * while(4/2 >0){
+   *   count += (4/2) * (2/2)
+   *   => count += (2 * 1 = 2
+   *   => count = 0+2 =2
+   *   ----
+   *   if(4 % 2 > 2/2){ // true
+   *     count += (4 % 2) - 2 / 2
+   *     =>count +=  0 - 1
+   *     =>count =  2-1=1
+   *   }else{
+   *      count += 0
+   *      => count = 2 +0=2
+   *   }
+   *   loop end: count = 2
+   * }
+   * -------
+   * p=4,
+   * while(4/4 >0){
+   *   count += (4/4) * (4/2)
+   *   => count += 1 * 2 = 2
+   *   => count = 2+2 =4
+   *   ----
+   *   if(4 % 2 > 4/2){ // false
+   *     count += (4 % 2) - 2 / 2
+   *     =>count +=  0 - 1
+   *     =>count =  2-1=1
+   *   }else{
+   *      count += 0
+   *      => count = 2 +0=2
+   *   }
+   *   loop end: count = 2
+   * }
+   * @param num
+   */
+  public static long solve_scalar_editorial(int num) {
+    long count = 0;
+    long p = 2;
+    num = num + 1;
+
+    while (num / p > 0) {
+      count += (num / p) * (p / 2);
+      count += (num % p > p / 2) ? (num % p) - p / 2 : 0;
+      p *= 2;
+    }
+
+    count += (num % p > p / 2) ? (num % p) - p / 2 : 0;
+
+    return count % 1000000007;
+  }
+
+  /**
+   * Brian Kernighan’s algorithm
+   *
+   * @param A
+   * @return
+   */
+  public long solve_brian_kernighan_algo(int A) {
+    long count = 0;
+
+    for (int i = 1; i <= A; i++) {
+      int n = i;
+      while (n != 0) {
+        n = n & (n - 1);
+        count++;
+      }
     }
 
     return count % 1000000007;
